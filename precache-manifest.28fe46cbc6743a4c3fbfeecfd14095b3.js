165306a07a9f41d1924ee33de3c074f9c3e23d7c
bondarenko-vlad <vladbondarenko173@gmail.com>
2020-11-13 17:07:59 +0300

    Updates

---
+++ precache-manifest.28fe46cbc6743a4c3fbfeecfd14095b3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ddfbde8f5713c46bc5aef11e5ffc3f",
+    "url": "/helloW/index.html"
+  },
+  {
+    "revision": "e8bd4ef0167562a70876",
+    "url": "/helloW/static/css/main.b88508e7.chunk.css"
+  },
+  {
+    "revision": "eae5c2bc99c99c315d58",
+    "url": "/helloW/static/js/2.8fbc8f00.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/helloW/static/js/2.8fbc8f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8bd4ef0167562a70876",
+    "url": "/helloW/static/js/main.4b19b9f9.chunk.js"
+  },
+  {
+    "revision": "91ba182edc28a98887b4",
+    "url": "/helloW/static/js/runtime-main.622f9d5d.js"
+  },
+  {
+    "revision": "a151f81c8ac05402557ab4f7f19dd815",
+    "url": "/helloW/static/media/arny.a151f81c.jpg"
+  },
+  {
+    "revision": "90e9829cccd3afc2c5c2769db63c9fa9",
+    "url": "/helloW/static/media/bill.90e9829c.jpg"
+  },
+  {
+    "revision": "f43a7c50f6a85373d898b3084d917b30",
+    "url": "/helloW/static/media/elon.f43a7c50.jpg"
+  },
+  {
+    "revision": "0ddca7a6d3d485bc11baac1505e5e593",
+    "url": "/helloW/static/media/ivan.0ddca7a6.jpg"
+  }
+]);
